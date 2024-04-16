464f766c452bf06f36eb0a2769ba7cdc6bea77ef
n3ndor <107216827+n3ndor@users.noreply.github.com>
2024-04-16 10:50:51 -0400

    game updated, now we will draw 2 random cards

--- Blackjack/views.py
+++ Blackjack/views.py
@@ -2,14 +2,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import random
 
+def get_random_card():
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    suit = random.choice(suits)
+    rank = random.choice(ranks)
+    return f"{rank} of {suit}"
+
 
 @api_view(['GET', 'POST'])
 def BlackjackView(request):
     if request.method == 'POST':
-        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-        suit = random.choice(suits)
-        rank = random.choice(ranks)
-        card = f"{rank} of {suit}"
-        return Response({"card": card})
+        card_one = get_random_card()
+        card_two = get_random_card()
+        return Response({"card": [card_one, card_two]})
     return Response({"message": "POST request to get a random card."})
--- frontend/app/blackjack/page.tsx
+++ frontend/app/blackjack/page.tsx
@@ -4,9 +4,9 @@
 import { useState } from 'react';
 
 export default function Blackjack() {
-  const [card, setCard] = useState('');
+  const [card, setCard] = useState<string[]>([]);
 
-  const fetchRandomCard = async () => {
+  const fetchCard = async () => {
     const response = await fetch('http://localhost:8000/blackjack/', {
       method: 'POST',
       headers: {
@@ -18,9 +18,22 @@ export default function Blackjack() {
   };
 
   return (
-    <div>
-      <button onClick={fetchRandomCard} className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-3 px-6 rounded">Get Random Card</button>
-      <p>{card}</p>
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <div className="w-full max-w-md bg-white shadow-md rounded-xl px-8 pt-6 pb-8 mx-4 sm:mx-0">
+        <h1 className="text-3xl font-bold text-center mb-8">Blackjack Game</h1>
+        <button onClick={fetchCard} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block w-full text-center">
+          Draw Cards
+        </button>
+        {card.length > 0 && (
+          <div className="space-y-4 mt-4">
+            {card.map((card, index) => (
+              <p key={index}>
+                {card}
+              </p>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
